b759dc20ae4b6178b54c6d93ccfd1ee0a930de18
akaitoraxd <sonifadilah024@gmail.com>
2023-09-27 17:23:38 +0700

    Jurnal01

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,5 +1,7 @@
 public class Jurnal01{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Nama: Soni Fadhilah");
+        System.out.println("Nim: 607062300132");
+        System.out.println("Kelas: D3-RPLA-01");
     }
 }
